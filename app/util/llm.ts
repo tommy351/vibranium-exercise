4d3e1b0437514c14f07602e2a0a5df754fe428a7
Jia-Huei Chen <tommy351@gmail.com>
2025-04-02 22:20:27 +0800

    Trim messages

--- app/util/llm.ts
+++ app/util/llm.ts
@@ -6,9 +6,14 @@ import {
 } from "@langchain/langgraph";
 import { PostgresSaver } from "@langchain/langgraph-checkpoint-postgres";
 import { ChatOpenAI } from "@langchain/openai";
+import { trimMessages } from "@langchain/core/messages";
 import { pool } from "./db";
 
 const NODE_MODEL = "model";
+const SYSTEM_MESSAGE = {
+  role: "system",
+  content: "You are a helpful assistant.",
+};
 
 const llm = new ChatOpenAI({
   model: "gpt-4o",
@@ -18,9 +23,17 @@ const checkpointer = new PostgresSaver(pool);
 
 await checkpointer.setup();
 
+const trimmer = trimMessages({
+  strategy: "last",
+  maxTokens: 100,
+  tokenCounter: (msgs) => msgs.length,
+});
+
 const workflow = new StateGraph(MessagesAnnotation)
   .addNode(NODE_MODEL, async (state) => {
-    const response = await llm.invoke(state.messages);
+    const trimmed = await trimmer.invoke(state.messages);
+    const response = await llm.invoke([SYSTEM_MESSAGE, ...trimmed]);
+
     return { messages: response };
   })
   .addEdge(START, NODE_MODEL)
